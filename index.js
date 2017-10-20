930aeef3a79da909758a6451b5f7ddf31d3e4273
Serkan Aysan <serkanaysann@gmail.com>
2017-10-20 14:35:11 +0300

    index.js fix

--- index.js
+++ index.js
@@ -1,39 +1,28 @@
 var express = require('express');
-var socket = require('socket.io');
+var bodyParser = require('body-parser');
 
-
-//App setup
 var app = express();
-var server = app.listen(8080, function(){
-	console.log('listening to request on port 8080');
-});
 
-//Static files
 app.use(express.static('public'));
 
-//Socket setup
-var io = socket(server);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
 
-io.on('connection', function(socket){
+//Initialize routes
+app.use('/api', require('./routes/api'));
 
-	socket.on('joinRoom', function(data){
-		socket.room = data.code;
-		socket.join(data.code);
-		socket.user = data.user;
-	});
 
-	socket.on('changeTemp', function(data){
-		io.to(socket.room).emit('changeTemp', data);
-	});
-
-	socket.on('turnOnOff', function(data){
-		io.to(socket.room).emit('turnOnOff', data);
-	});
-
-	socket.on('disconnect', function () {
-
-      
-  });
-
+app.get('/',function(req, res){
+	console.log('get request');
+	res.end();
 });
 
+
+
+app.listen(process.env.port || 4000, function(){
+	console.log('now listening for request');
+});
+
+/*meantraDB
+meantra_root
+y83SmUY0*/
